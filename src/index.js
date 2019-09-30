1d769d09a2514033c11ae106bf1e933d075b97ae
stacy <chiufin@hotmail.com>
2019-09-30 20:07:01 +0100

    pass all test using recursive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export function parse(messages) {
-    return messages;
+
+export function parse(messages, list = '') {
+    if(typeof messages == 'string'){
+        list+= messages
+    }
+
+    if(typeof messages == 'object'){
+        list += '<ul>'
+        for(let msg in messages){
+            list += '<li>'
+            list = `${parse(messages[msg], list)}</li>`
+        }
+        list += '</ul>'
+    } 
+    return list
 }
